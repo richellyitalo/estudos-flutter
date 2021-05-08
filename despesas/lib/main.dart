1f547ca94b1ecc1b6901c9b199a4ec08e9360f83
Richelly Italo <richellyitalo@gmail.com>
2021-05-08 15:15:57 -0300

    Listagem de registros; Inputs de nova transação

--- despesas/lib/main.dart
+++ despesas/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './models/transaction.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -10,8 +12,122 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class HomeApp extends StatelessWidget {
+  final List<Transaction> _transactions = [
+    Transaction(
+        id: 't1',
+        title: 'Compras mercado',
+        value: 450.55,
+        date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Mensalidade Internet',
+        value: 350.00,
+        date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('App de despesas', textAlign: TextAlign.center,));
+    return Scaffold(
+        appBar: AppBar(title: Text('Despesas semanais')),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                color: Colors.greenAccent,
+                child: Text(
+                  'Gráfico de despesas',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: _transactions.map((tr) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        child: Text(
+                          'R\$ ${tr.value.toStringAsFixed(2)}',
+                          style: TextStyle(
+                            color: Colors.purple,
+                            fontSize: 20,
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tr.title,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text(
+                            DateFormat('d MMM y').format(tr.date),
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+            Card(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Valor',
+                      ),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Nova Transação',
+                            style: TextStyle(color: Colors.purple),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              elevation: 5,
+            )
+          ],
+        ));
   }
 }
